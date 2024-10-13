6bed631d9eed1f7a636f92354242d97cc0156d77
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2024-10-13 15:53:18 +0400

    commit

--- Day 01/classwork/classwork.py
+++ Day 01/classwork/classwork.py
@@ -2,3 +2,4 @@ name = "vato"
 surname = "imnaishvili"
 print(name)
 print(name + "aris" + surname)
+vato is ugly = False
---
+++ Day 01/homework/homework.py
@@ -0,0 +1,6 @@
+name = "vato"
+surname = "imnaishvili"
+age = 16
+height = 177
+study = "coding"
+print("my name is" + name +"my surname is" + surname + "my age is" + str(age) + "my height is" + str(height) + "i'm studying" + study )
